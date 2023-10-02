daab672d64e621bb30e429fc433c5c688f1c9381
Lucas Romão <lucas.gabryell@souunit.com.br>
2023-10-02 12:44:43 -0300

    Inital commit

---
+++ graphicsFunctions.py
@@ -0,0 +1,118 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+def plot_manual_time(dados):
+    # Adicione o filtro de Departamento com a opção "Todos"
+    departamentos = ['Todos'] + list(dados['Department'].unique())
+    departamento_selecionado = st.selectbox('Selecione um Departamento', departamentos)
+
+    # Filtrar os dados com base no departamento selecionado, se não for "Todos"
+    if departamento_selecionado != 'Todos':
+        dados_filtrados = dados[dados['Department'] == departamento_selecionado]
+        st.write(f"### Gráfico ManualTime para o Departamento: {departamento_selecionado}")
+    else:
+        dados_filtrados = dados
+        st.write(f"### Gráfico ManualTime para Todos os Departamentos")
+
+    # Agrupe os dados por ReleaseName e calcule a média do ManualTime
+    media_manual_time = dados_filtrados.groupby('ReleaseName')['ManualTime'].mean().reset_index()
+
+    # Crie o gráfico de barras
+    fig = px.bar(media_manual_time, x='ReleaseName', y='ManualTime',
+                 labels={'ReleaseName': 'Nome da Automação', 'ManualTime': 'Média Manual Time (segundos)'})
+
+    st.plotly_chart(fig)
+
+def plot_rpa_time(dados):
+    # Adicione o filtro de Departamento com a opção "Todos"
+    departamentos = ['Todos'] + list(dados['Department'].unique())
+    departamento_selecionado = st.selectbox('Selecione um Departamento', departamentos)
+
+    # Filtrar os dados com base no departamento selecionado, se não for "Todos"
+    if departamento_selecionado != 'Todos':
+        dados_filtrados = dados[dados['Department'] == departamento_selecionado]
+        st.write(f"### Gráfico de Tempo de execução RPA para o Departamento: {departamento_selecionado}")
+    else:
+        dados_filtrados = dados
+        st.write(f"### Gráfico de Tempo de execução RPA para Todos os Departamentos")
+
+    # Agrupe os dados por ReleaseName e calcule a média do ManualTime
+    media_manual_time = dados_filtrados.groupby('ReleaseName')['Tempo de execução RPA (Segundos)'].mean().reset_index()
+
+    # Crie o gráfico de barras
+    fig = px.bar(media_manual_time, x='ReleaseName', y='Tempo de execução RPA (Segundos)',
+                 labels={'ReleaseName': 'Nome da Automação', 'Tempo de execução RPA (Segundos)': 'Média RPA Time (segundos)'})
+
+    st.plotly_chart(fig)
+
+def compare_manualtime_rpa_time(dados):
+    # Adicione o filtro de Departamento com a opção "Todos"
+    departamentos = ['Todos'] + list(dados['Department'].unique())
+    departamento_selecionado = st.selectbox('Selecione um Departamento', departamentos)
+
+    # Filtrar os dados com base no departamento selecionado, se não for "Todos"
+    if departamento_selecionado != 'Todos':
+        dados_filtrados = dados[dados['Department'] == departamento_selecionado]
+        st.write(f"### Comparação entre ManualTime e Tempo de Execução RPA para o Departamento: {departamento_selecionado}")
+    else:
+        dados_filtrados = dados
+        st.write(f"### Comparação entre ManualTime e Tempo de Execução RPA para Todos os Departamentos")
+
+    # Calcula a média de ManualTime e RPA Time para cada ReleaseName
+    media_manual_time = dados_filtrados.groupby('ReleaseName')['ManualTime'].mean().reset_index()
+    media_rpa_time = dados_filtrados.groupby('ReleaseName')['Tempo de execução RPA (Segundos)'].mean().reset_index()
+
+    # Une os dados de ManualTime e RPA Time com base em ReleaseName
+    dados_comparacao = pd.merge(media_manual_time, media_rpa_time, on='ReleaseName', how='inner')
+
+    # Renomeia as colunas
+    dados_comparacao = dados_comparacao.rename(columns={'ManualTime': 'Média Manual Time (segundos)',
+                                                      'Tempo de execução RPA (Segundos)': 'Média Tempo de Execução RPA (segundos)'})
+
+    # Crie o gráfico de barras empilhadas
+    fig = px.bar(dados_comparacao, x='ReleaseName', y=['Média Manual Time (segundos)', 'Média Tempo de Execução RPA (segundos)'],
+                 labels={'ReleaseName': 'Nome da automação', 'value': 'Tempo Médio (segundos)'},
+                 title='Comparação entre ManualTime e Tempo de Execução RPA',
+                 barmode='group')
+
+    st.plotly_chart(fig)
+
+    # Calcule o tempo total utilizado manual e o tempo total utilizado RPA
+    total_manual_time = dados_comparacao['Média Manual Time (segundos)'].sum()
+    total_rpa_time = dados_comparacao['Média Tempo de Execução RPA (segundos)'].sum()
+
+    # Exiba o tempo total em forma de texto
+    st.write(f"Tempo Total Utilizado Manualmente: {total_manual_time:.2f} segundos")
+    st.write(f"Tempo Total Utilizado com RPA: {total_rpa_time:.2f} segundos")
+    st.write(f"Tempo Total otimizado com RPA: {total_manual_time - total_rpa_time:.2f} segundos")
+
+
+def plot_horas_retornadas(dados):
+    # Adicione o filtro de Departamento com a opção "Todos"
+    departamentos = ['Todos'] + list(dados['Department'].unique())
+    departamento_selecionado = st.selectbox('Selecione um Departamento', departamentos)
+
+    # Filtrar os dados com base no departamento selecionado, se não for "Todos"
+    if departamento_selecionado != 'Todos':
+        dados_filtrados = dados[dados['Department'] == departamento_selecionado]
+        st.write(f"### Horas Retornadas para o Departamento: {departamento_selecionado}")
+    else:
+        dados_filtrados = dados
+        st.write(f"### Horas Retornadas para Todos os Departamentos")
+
+    # Agrupe os dados por ReleaseName e calcule a soma das Horas Retornadas
+    horas_retornadas = dados_filtrados.groupby('ReleaseName')['Horas Retornadas'].sum().reset_index()
+
+    # Arredonde os valores para números inteiros
+    horas_retornadas['Horas Retornadas'] = horas_retornadas['Horas Retornadas'].round().astype(int)
+
+    # Crie o gráfico de barras empilhadas
+    fig = px.bar(horas_retornadas, x='ReleaseName', y='Horas Retornadas',
+                 labels={'ReleaseName': 'Noma da automação', 'Horas Retornadas': 'Horas Retornadas'},
+                 title='Horas Retornadas',
+                 text='Horas Retornadas')
+
+    st.plotly_chart(fig)
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import graphicsFunctions
+
+# Título do aplicativo
+st.title("Automações Maxicrédito")
+
+# Substitua 'seu_arquivo.csv' pelo caminho real do seu arquivo CSV
+dadosRpaCsv = 'dadosRPA.csv'
+
+# Use o método 'read_csv' do pandas para ler o arquivo CSV
+dadosRPA = pd.read_csv(dadosRpaCsv)
+
+# Exibir os dados no Streamlit
+st.write("### Dados do RPA:")
+st.write(dadosRPA)
+
+# Barra de seleção para escolher o gráfico
+opcao_grafico = st.selectbox("Escolha o Gráfico", ["Gráfico de Tempo Manual",
+                                                   "Gráfico de Tempo de execução RPA (Segundos)",
+                                                   "Gráfico Manual Time X RPA Time", "Gráfico de Horas Retornadas"])
+
+# Mostrar o gráfico selecionado com base na escolha do usuário
+if opcao_grafico == "Gráfico de Tempo Manual":
+    graphicsFunctions.plot_manual_time(dadosRPA)
+elif opcao_grafico == "Gráfico de Tempo de execução RPA (Segundos)":
+    graphicsFunctions.plot_rpa_time(dadosRPA)
+elif opcao_grafico == "Gráfico Manual Time X RPA Time":
+    graphicsFunctions.compare_manualtime_rpa_time(dadosRPA)
+elif opcao_grafico == "Gráfico de Horas Retornadas":
+    graphicsFunctions.plot_horas_retornadas(dadosRPA)
